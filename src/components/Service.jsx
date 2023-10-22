75d0dabb8b410cd5d3f3c42e1d6dc21faee74cfe
EmonArefin <shovon1218@gmail.com>
2023-10-23 01:42:28 +0600

    Basic project ready

--- src/components/Service.jsx
+++ src/components/Service.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 
 const Service = () => {
+    //Show how JSX Conditional Rendering Using Ternary Operator works
+    const serviceProject = 4;
     return (
         <div>
             <h2>Service Section</h2>
             <p>This is me at your service</p>
+            {serviceProject<5?(<p>I can serve you all project</p>):(<p>This is too much project for me</p>)}
         </div>
     );
 };
