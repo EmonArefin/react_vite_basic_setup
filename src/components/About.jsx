98e2f16713a33f60703d40fe59c18ada425600a0
EmonArefin <shovon1218@gmail.com>
2023-10-23 01:22:06 +0600

    applying if-else and loop

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,10 +1,19 @@
 import React from 'react';
 
 const About = () => {
+    //Show how Loop works in JSX
+    const contactMe = ["01400264525", "https://github.com/EmonArefin", "contact.emonarefin@gmail.com"]
     return (
         <div>
             <h2>About Section</h2>
             <p>This is about me - Emon Arefin</p>
+            <ol>
+                {
+                    contactMe.map((item)=>{
+                        return(<li>{item}</li>)
+                    })
+                }
+            </ol>
         </div>
     );
 };
--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 
 const Project = () => {
+    //Use Short Hand If Else in any JSX.
+    const myProject = 11;
     return (
         <div>
             <h2>Project Section</h2>
             <p>This is a react basic project</p>
+            {myProject>=11?(<p>Running and future project</p>):(<p>Project passed</p>)}
         </div>
     );
 };
